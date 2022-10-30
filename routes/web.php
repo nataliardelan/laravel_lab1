0efac911c2ee18f725a7b5267de732598d96ca20
natali <48475980+nataliardelan@users.noreply.github.com>
2022-10-30 22:45:35 +0200

    lab2

--- routes/web.php
+++ routes/web.php
@@ -18,25 +18,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Route::resource('users', UserController::class);
+Route::resource('users', UserController::class)
+    ->middleware('auth');
 
-Route::get('/users', [UserController::class, 'index'])
-    ->name('users.index');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/users/create', [UserController::class, 'create'])
-    ->name('users.create');
-
-Route::post('/users/', [UserController::class, 'store'])
-    ->name('users.store');
-
-Route::get('/users/{user}', [UserController::class, 'show'])
-    ->name('users.show');
-
-Route::get('/users/{user}/edit', [UserController::class, 'edit'])
-    ->name('users.edit');
-
-Route::put('/users/{user}', [UserController::class, 'update'])
-    ->name('users.update');
-
-Route::delete('/users/{user}', [UserController::class, 'destroy'])
-    ->name('users.destroy');
+require __DIR__.'/auth.php';
