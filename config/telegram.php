1adb1b6c62b298721b0d546359be31753cd178d9
natali <48475980+nataliardelan@users.noreply.github.com>
2022-11-13 19:49:00 +0200

    lab_v3

--- config/telegram.php
+++ config/telegram.php
@@ -34,7 +34,7 @@ return [
             'certificate_path'    => env('TELEGRAM_CERTIFICATE_PATH', 'YOUR-CERTIFICATE-PATH'),
             'webhook_url'         => env('TELEGRAM_WEBHOOK_URL', 'YOUR-BOT-WEBHOOK-URL'),
             'commands'            => [
-                App\Bot\Commands\StartCommand::class
+                App\bot\commands\StartCommand::class
             ],
         ],
 
